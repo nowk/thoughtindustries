b62bd5604a1c1750ce89d3a21aef81401402c85a
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2018-07-05 20:26:30 -0400

    Add Course views call

--- apis/notifications/course.go
+++ apis/notifications/course.go
@@ -10,8 +10,14 @@ type CoursePurchases struct {
 	Events   []Purchase `json:"events"`
 }
 
+type CourseViews struct {
+	PageInfo PageInfo `json:"pageInfo"`
+	Views    []View   `json:"events"`
+}
+
 type Courser interface {
 	Purchases() (*CoursePurchases, error)
+	Views() (*CourseViews, error)
 }
 
 type course struct {
@@ -22,6 +28,17 @@ func (c *course) Purchases() (*CoursePurchases, error) {
 	var data CoursePurchases
 
 	err := c.Get("coursePurchase").Json(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+func (c *course) Views() (*CourseViews, error) {
+	var data CourseViews
+
+	err := c.Get("courseView").Json(&data)
 	if err != nil {
 		return nil, err
 	}
